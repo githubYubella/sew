d5b862ed546f5fee4f0d9ac4bbbb6e1b9a56b13d
githubYubella <anmpbella@gmail.com>
2022-05-18 23:56:06 +0700

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  login_email:string=""
+  login_password:string=""
+  email:string=""
+  password:string=""
+
+  async login(){
+    this.email=this.login_email
+    this.password=this.login_password
+    alert("Login Success");
+  }
+  constructor( private storage:Storage) {}
+
+  async ngOnInit(){
+    await this.storage.create();
+    this.email = await this.storage.get('email');
+    this.login_email = await this.storage.get('login_email')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [[BrowserModule, IonicModule.forRoot(), AppRoutingModule],FormsModule,IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
